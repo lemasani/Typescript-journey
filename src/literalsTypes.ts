83a1675d8151f2302a3f2bbf7c5c79b93defc9f5
lemasani <brianlemasan@gmail.com>
2024-09-02 16:10:59 +0300

    literals types

---
+++ src/literalsTypes.ts
@@ -0,0 +1,10 @@
+// used to limit values by specificy an exact or specific
+
+type Quantity = 50 | 100
+
+
+let quantity: Quantity = 100
+
+type  Metric = 'cm' | 'inch';
+
+let metric: Metric = 'cm'
