da19f9db5412b3ce8638859f4264c82c4a61bcbf
lemasani <brianlemasan@gmail.com>
2024-09-03 00:14:46 +0300

    index signature

---
+++ src/indexSignatures.ts
@@ -0,0 +1,13 @@
+
+class SeatAssignment {
+    // index signature property
+    [seatNumber: string]: string
+
+
+
+}
+
+
+let seats = new SeatAssignment()
+seats['A1'] = 'Alice' // or
+seats.B1 = 'Max'
