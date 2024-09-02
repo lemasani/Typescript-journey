915de3e3f94863b3df0bf955dddef8749e88c63b
lemasani <brianlemasan@gmail.com>
2024-09-03 00:02:13 +0300

    acess modifiers

--- dist/classes.js
+++ dist/classes.js
@@ -4,16 +4,20 @@ class Account {
     constructor(id, owner, balance) {
         this.id = id;
         this.owner = owner;
-        this.balance = balance;
+        this._balance = balance;
     }
     deposit(amount) {
         if (amount <= 0) {
             throw new Error('Invalid amount');
         }
-        this.balance += amount;
+        this._balance += amount;
+    }
+    getBalance() {
+        return this._balance;
     }
 }
+// creating object
 let account = new Account(1, 'John doe', 0);
 account.deposit(100);
-console.log(account instanceof Account); // 100
+console.log(account.getBalance()); // 100
 //# sourceMappingURL=classes.js.map
--- src/classes.ts
+++ src/classes.ts
@@ -4,27 +4,29 @@
 class Account {
     readonly id: number
     owner: string
-    balance: number
+    private _balance: number
     nickname?: string // makes this property optional and no need to add it on the constructor
 
     constructor(id: number, owner: string, balance: number){
         this.id = id
         this.owner = owner
-        this.balance = balance
+        this._balance = balance
     }
 
     deposit(amount:number): void{
         if(amount <= 0){
             throw new Error('Invalid amount')
         }
-        this.balance += amount
+        this._balance += amount
     }
 
-
+    getBalance(): number {
+        return this._balance
+    }
 }
 
 // creating object
 let account = new Account(1, 'John doe', 0)
 account.deposit(100)
 
-console.log(account.balance) // 100
+console.log(account.getBalance()) // 100
